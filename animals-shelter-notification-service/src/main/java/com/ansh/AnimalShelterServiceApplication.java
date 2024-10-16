dd0da1c1f3e99af37735287524c4acda866f757b
NatOlk <nolkoeva@gmail.com>
2024-10-16 17:02:20 +0200

    Fix wrong package

--- animals-shelter-notification-service/src/main/java/com/ansh/AnimalShelterServiceApplication.java
+++ animals-shelter-notification-service/src/main/java/com/ansh/AnimalShelterServiceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication(scanBasePackages = "com.example.gr")
+@SpringBootApplication(scanBasePackages = "com.ansh")
 @Configuration
 public class AnimalShelterServiceApplication
 {
