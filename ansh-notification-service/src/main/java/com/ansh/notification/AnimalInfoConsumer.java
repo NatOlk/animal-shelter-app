b3dc6ddc593accfd8515011d6b2495ff6578d739
NatOlk <nolkoeva@gmail.com>
2024-10-22 18:12:14 +0200

    Rename projects

--- ansh-notification-service/src/main/java/com/ansh/notification/AnimalInfoConsumer.java
+++ ansh-notification-service/src/main/java/com/ansh/notification/AnimalInfoConsumer.java
@@ -6,15 +6,19 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AnimalInfoConsumer {
+  @Value("${animalGroupTopicId}")
+  private String animalGroupTopicId;
+
   @Autowired
   private AnimalEventHandlerRegistry handlerRegistry;
 
-  @KafkaListener(topics = "animalGroupId", groupId = "animalGroupId")
+  @KafkaListener(topics = "${animalGroupTopicId}", groupId = "animalGroupId")
   public void listen(ConsumerRecord<String, String> record) throws IOException {
 
     AnimalEvent animalEvent = new ObjectMapper().readValue(record.value(), AnimalEvent.class);
--- ansh-service/src/main/java/com/ansh/AnimalShelterApp.java
+++ ansh-service/src/main/java/com/ansh/AnimalShelterApp.java
@@ -3,12 +3,14 @@ package com.ansh;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.SpringSecurityCoreVersion;
 
 @SpringBootApplication(scanBasePackages = "com.ansh")
 @Configuration
 public class AnimalShelterApp {
 
   public static void main(String[] args) {
+    System.out.println("Spring Security version: " + SpringSecurityCoreVersion.getVersion());
     SpringApplication.run(AnimalShelterApp.class, args);
   }
 }
