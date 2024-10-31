5a3ce9c85d0162e36c2fe02cbac68cc759b32dea
NatOlk <nolkoeva@gmail.com>
2024-10-31 20:27:17 +0100

    Rename topics ids

--- ansh-service/src/main/java/com/ansh/notification/AnimalInfoProducer.java
+++ ansh-service/src/main/java/com/ansh/notification/AnimalInfoProducer.java
@@ -4,6 +4,7 @@ import com.ansh.event.AnimalEvent;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +12,8 @@ import org.springframework.stereotype.Component;
 public class AnimalInfoProducer {
 
   private static final Logger LOG = LoggerFactory.getLogger(AnimalInfoProducer.class);
+  @Value("${animalTopicId}")
+  private String animalTopicId;
 
   private final KafkaTemplate<String, String> kafkaTemplate;
   private final ObjectMapper objectMapper;
@@ -21,10 +24,10 @@ public class AnimalInfoProducer {
     this.objectMapper = objectMapper;
   }
 
-  public void sendMessage(String topic, AnimalEvent animalEvent) {
+  public void sendMessage(AnimalEvent animalEvent) {
     try {
       String jsonMessage = objectMapper.writeValueAsString(animalEvent);
-      kafkaTemplate.send(topic, jsonMessage);
+      kafkaTemplate.send(animalTopicId, jsonMessage);
     } catch (Exception e) {
       LOG.error("Exception during sending message:", e.getMessage());
     }
