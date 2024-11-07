08571b1319932e923ec028df6c5dd859d6974235
NatOlk <nolkoeva@gmail.com>
2024-11-07 15:16:45 +0100

    SonarLint fixes

--- ansh-notification-service/src/main/java/com/ansh/notification/handler/SubscriptionMessages.java
+++ ansh-notification-service/src/main/java/com/ansh/notification/handler/SubscriptionMessages.java
@@ -1,18 +1,19 @@
 package com.ansh.notification.handler;
 
-public interface SubscriptionMessages {
+public final class SubscriptionMessages {
+  private SubscriptionMessages(){}
 
-  String ADD_ANIMAL_TEMPLATE = "addAnimalTemplate";
-  String REMOVE_ANIMAL_TEMPLATE = "removeAnimalTemplate";
-  String ADD_VACCINE_TEMPLATE = "addVaccineTemplate";
-  String REMOVE_VACCINE_TEMPLATE = "removeVaccineTemplate";
-  String ADD_ANIMAL_SUBJECT = "[animal-shelter-app] New animal added";
-  String REMOVE_ANIMAL_SUBJECT = "[animal-shelter-app] Animal removed";
-  String ADD_VACCINE_SUBJECT = "[animal-shelter-app] New vaccine added";
-  String REMOVE_VACCINE_SUBJECT = "[animal-shelter-app] Vaccine removed";
-  String SUBSCRIPTION_SUBJECT = "[animal-shelter-app] Please accept subscription for Animal Shelter app";
-  String SUCCESS_SUBSCRIPTION_SUBJECT = "[animal-shelter-app] Subscription for Animal Shelter app";
-  String ACCEPT_SUBSCRIPTION_TEMPLATE = "acceptSubscription";
-  String SUCCESS_SUBSCRIPTION_TEMPLATE = "successSubscription";
-  String REPEAT_SUBSCRIPTION_TEMPLATE = "repeatSubscription";
+  public static final String ADD_ANIMAL_TEMPLATE = "addAnimalTemplate";
+  public static final String REMOVE_ANIMAL_TEMPLATE = "removeAnimalTemplate";
+  public static final String ADD_VACCINE_TEMPLATE = "addVaccineTemplate";
+  public static final String REMOVE_VACCINE_TEMPLATE = "removeVaccineTemplate";
+  public static final String ADD_ANIMAL_SUBJECT = "[animal-shelter-app] New animal added";
+  public static final String REMOVE_ANIMAL_SUBJECT = "[animal-shelter-app] Animal removed";
+  public static final String ADD_VACCINE_SUBJECT = "[animal-shelter-app] New vaccine added";
+  public static final String REMOVE_VACCINE_SUBJECT = "[animal-shelter-app] Vaccine removed";
+  public static final String SUBSCRIPTION_SUBJECT = "[animal-shelter-app] Please accept subscription for Animal Shelter app";
+  public static final String SUCCESS_SUBSCRIPTION_SUBJECT = "[animal-shelter-app] Subscription for Animal Shelter app";
+  public static final String ACCEPT_SUBSCRIPTION_TEMPLATE = "acceptSubscription";
+  public static final String SUCCESS_SUBSCRIPTION_TEMPLATE = "successSubscription";
+  public static final String REPEAT_SUBSCRIPTION_TEMPLATE = "repeatSubscription";
 }
