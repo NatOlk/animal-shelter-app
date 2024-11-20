7a89b6e0859bb92737e12a4e94f82025e3071d56
NatOlk <nolkoeva@gmail.com>
2024-11-20 19:08:01 +0100

    [auth] Refactor login to use token authentication instead of working with JSESSIONID cookies, enabling the service to be used as a microservice.

--- animals-shelter-react/src/common/authContext.js
+++ animals-shelter-react/src/common/authContext.js
@@ -1,23 +1,38 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useState, useEffect } from "react";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
     const [user, setUser] = useState({ id: null, email: null });
+    const [isLoading, setIsLoading] = useState(true);
 
-    const login = (userData) => {
+    useEffect(() => {
+        const token = localStorage.getItem('jwt');
+
+        if (token !== 'undefined' && token !== null && token !== '')  {
+            setIsAuthenticated(true);
+        } else {
+            setIsAuthenticated(false);
+        }
+
+        setIsLoading(false);
+    }, []);
+
+    const login = (userData, token) => {
         setIsAuthenticated(true);
         setUser(userData);
+        localStorage.setItem('jwt', token);
     };
 
     const logout = () => {
         setIsAuthenticated(false);
         setUser({ id: null, email: null });
+        localStorage.removeItem('jwt');
     };
 
     return (
-        <AuthContext.Provider value={{ isAuthenticated, user, login, logout }}>
+        <AuthContext.Provider value={{ isAuthenticated, user, login, logout, isLoading }}>
             {children}
         </AuthContext.Provider>
     );
---
+++ ansh-service/src/main/java/com/ansh/auth/service/JwtService.java
@@ -0,0 +1,60 @@
+package com.ansh.auth.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.function.Function;
+import javax.crypto.SecretKey;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+  @Value("${jwt.secret.key}")
+  private String jwtSecretKey;
+
+  public String extractUsername(String token) {
+    return extractClaim(token, Claims::getSubject);
+  }
+
+  public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+    final Claims claims = extractAllClaims(token);
+    return claimsResolver.apply(claims);
+  }
+
+  private Claims extractAllClaims(String token) {
+    return Jwts.parserBuilder()
+        .setSigningKey(getSigningKey())
+        .build()
+        .parseClaimsJws(token)
+        .getBody();
+  }
+
+  public String generateToken(String username) {
+    return Jwts.builder()
+        .setClaims(new HashMap<>())
+        .setSubject(username)
+        .setIssuedAt(new Date(System.currentTimeMillis()))
+        .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 8))
+        .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+        .compact();
+  }
+
+  private SecretKey getSigningKey() {
+    return Keys.hmacShaKeyFor(jwtSecretKey.getBytes());
+  }
+
+  public boolean isTokenValid(String token, UserDetails userDetails) {
+    final String username = extractUsername(token);
+    return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+  }
+
+  private boolean isTokenExpired(String token) {
+    return extractClaim(token, Claims::getExpiration).before(new Date());
+  }
+}
