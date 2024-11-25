6fc173e1a9ab2edfa5893a829feaecc17cc4df09
NatOlk <nolkoeva@gmail.com>
2024-11-25 13:09:46 +0100

    [react] Added name sorting and added selector to be able to select animals by species

--- ansh-service/src/main/java/com/ansh/repository/AnimalRepository.java
+++ ansh-service/src/main/java/com/ansh/repository/AnimalRepository.java
@@ -1,6 +1,7 @@
 package com.ansh.repository;
 
 import com.ansh.entity.animal.Animal;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,4 +13,5 @@ public interface AnimalRepository extends JpaRepository<Animal, Long> {
 
   @Query("SELECT v.animal FROM Vaccination v WHERE v.id = :vaccinationId")
   Animal findAnimalByVaccinationId(@Param("vaccinationId") @NonNull Long vaccinationId);
+  List<Animal> findAllByOrderByNameAsc();
 }
