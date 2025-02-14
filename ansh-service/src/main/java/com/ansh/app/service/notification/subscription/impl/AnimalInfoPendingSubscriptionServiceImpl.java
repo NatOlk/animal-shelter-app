2c51c1101c9bb9c49da81fa03939597b4c4817f6
NatOlk <nolkoeva@gmail.com>
2025-02-14 19:09:40 +0100

    [task] Document code and extract interfaces

--- ansh-service/src/main/java/com/ansh/app/service/notification/subscription/impl/AnimalInfoPendingSubscriptionServiceImpl.java
+++ ansh-service/src/main/java/com/ansh/app/service/notification/subscription/impl/AnimalInfoPendingSubscriptionServiceImpl.java
@@ -20,7 +20,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-@Service
+@Service("animalInfoPendingSubscriptionService")
 public class AnimalInfoPendingSubscriptionServiceImpl implements
     AnimalInfoPendingSubscriptionService {
 
