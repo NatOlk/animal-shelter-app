7d90cab8f8d272cc1a2691546230f256e03b5aba
NatOlk <nolkoeva@gmail.com>
2025-04-08 12:49:34 +0200

    [task] Implement new statistics service

---
+++ ansh-stats-service/src/main/kotlin/com/ansh/stats/controller/HelloController.kt
@@ -0,0 +1,13 @@
+package com.ansh.stats.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+
+    @GetMapping("/hello")
+    fun sayHello(): String {
+        return "Hello from Kotlin Spring Boot! 🚀"
+    }
+}
